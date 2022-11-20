d9fa47a71176350a4977c32eb7ee4b10a7265349
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2022-11-20 17:15:06 -0300

    feat: initial commit with mario run, jump and stop when jump fail

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+const floor = document.querySelector(".floor-container");
+
+function jump() {
+  if (mario.classList.contains("jump")) {
+    return;
+  }
+
+  mario.classList.add("jump");
+
+  setTimeout(() => {
+    mario.classList.remove("jump");
+  }, 500);
+}
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === " ") {
+    jump();
+  }
+});
+
+setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window
+    .getComputedStyle(mario)
+    .bottom.replace("px", "");
+  if (pipePosition <= 230 && pipePosition > 100 && marioPosition < 80) {
+    pipe.style.animation = "none";
+    floor.style.animation = "none";
+    mario.style.animation = "none";
+
+    pipe.style.left = `${pipePosition}px`;
+    mario.style.bottom = `${marioPosition}px`;
+  }
+}, 10);
